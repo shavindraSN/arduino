f1f0f119af1af8d625ca7668bf9bfef6e3a040c3
Manathunga, Shavindra <Shavindra.Manathunga@pearson.com>
2016-12-25 22:42:31 +0530

    Add complete source for hello world project

---
+++ hello_world/hello_world.ino
@@ -0,0 +1,21 @@
+// Initiate the pin
+int ledPin = 10;
+
+/*
+ * This method is run only once and use to initiate things used
+ * for rest of the program
+ */
+void setup() {
+  // Set the selected pin as a output pin
+  pinMode(ledPin, OUTPUT);
+}
+
+/*
+ * This function runs as long as Arduino is power on
+ */
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(50);
+  digitalWrite(ledPin, LOW);
+  delay(50);
+} 
